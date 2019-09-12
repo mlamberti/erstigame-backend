b9148e3cef171ab640c81953a2126d7915d928ea
Michael Lamberti <michael.lamberti@devolo.de>
2019-09-13 01:20:34 +0200

    cleanup some code

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -8,18 +8,15 @@ module Mutations
     field :errors, [String], null: true
 
     def resolve(name: nil, group_token: nil)
-
-      group = Group.find_by join_token: group_token
-
       user = User.new(
         name: name,
-        group: group
+        group: Group.find_by(join_token: group_token)
       )
 
       if context[:pundit].authorize(user, :create?).save
         { user: user, token: user.auth_token }
       else
-        { user: nil, errors: user.errors.full_messages }
+        { errors: user.errors.full_messages }
       end
     end
   end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,49 +8,5 @@ module Types
       context[:pundit].authorize context[:current_user], :show?
     end
 
-    field :all_users, [UserType], null: false
-    field :all_groups, [GroupType], null: false
-    field :all_hashtags, [HashtagType], null: false
-
-    field :user, UserType, null: true do
-      argument :id, ID, required: true
-    end
-
-    field :group, GroupType, null: true do
-      argument :id, ID, required: true
-    end
-
-    field :hashtag, HashtagType, null: true do
-      argument :id, ID, required: true
-    end
-
-    def all_users
-      context[:pundit].authorize User, :index?
-      context[:pundit].policy_scope User
-    end
-
-    def all_groups
-      Group.all
-    end
-
-    def all_hashtags
-      context[:pundit].authorize Hashtag, :index?
-      context[:pundit].policy_scope Hashtag
-    end
-
-    def user(id:)
-      user = context[:pundit].policy_scope(User).find(id)
-      context[:pundit].authorize user, :show?
-    end
-
-    def group(id:)
-      Group.find(id)
-    end
-
-    def hashtag(id:)
-      hashtag = Hashtag.find(id)
-      context[:pundit].authorize hashtag, :show?
-    end
-
   end
 end
