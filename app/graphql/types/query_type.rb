0f31ddae1f52a29ce286c92046d3d3607d118104
Michael Lamberti <michael.lamberti@devolo.de>
2019-09-24 21:26:34 +0200

    add user / group / hashtag queries again

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,5 +8,31 @@ module Types
       context[:pundit].authorize context[:current_user], :show?
     end
 
+    field :user, UserType, null: true do
+      argument :id, ID, required: true
+    end
+
+    def user(id:)
+      user = context[:pundit].policy_scope(User).find(id)
+      context[:pundit].authorize user, :show?
+    end
+
+    field :group, GroupType, null: true do
+      argument :id, ID, required: true
+    end
+
+    def group(id:)
+      Group.find(id)
+    end
+
+    field :hashtag, HashtagType, null: true do
+      argument :id, ID, required: true
+    end
+
+    def hashtag(id:)
+      hashtag = Hashtag.find(id)
+      context[:pundit].authorize hashtag, :show?
+    end
+
   end
 end
