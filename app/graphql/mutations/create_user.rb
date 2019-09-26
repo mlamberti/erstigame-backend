7a3be6a245b569c76665a333c99d3bee644213e7
Ben Schuermann <bennyschuermann@gmx.net>
2019-09-26 17:25:24 +0200

    fix gender

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -2,22 +2,22 @@ module Mutations
   class CreateUser < BaseMutation
     argument :name, String, required: true
     argument :group_token, String, required: true
-    argument :gender, String, required:false
+    argument :gender, Types::GenderEnum, required:false
     argument :info, String, required:false
 
 
-    field :token, String, null: true
+    field :auth_token, String, null: true
     field :user, Types::UserType, null: true
     field :errors, [String], null: true
 
-    def resolve(name: nil, group_token: nil)
+    def resolve(name:, group_token:, gender:nil, info:nil )
       user = User.new(
         name: name,
         group: Group.find_by(join_token: group_token)
       )
 
       if context[:pundit].authorize(user, :create?).save
-        { user: user, token: user.auth_token }
+        { user: user, auth_token: user.auth_token }
       else
         { errors: user.errors.full_messages }
       end
