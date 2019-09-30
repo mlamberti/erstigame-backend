9308fdd3e89e78779956122da11075e25a326727
Michael Lamberti <michael.lamberti@devolo.de>
2019-09-30 13:06:58 +0200

    ensure required level is above available level

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -5,6 +5,15 @@ class Hashtag < ApplicationRecord
   belongs_to :level, optional: true
   belongs_to :required_by_level, optional: true, class_name: "Level"
 
+  validate :not_required_before_available
+
+  def not_required_before_available
+    return unless self.required_by_level
+
+    errors.add(:required_by_level, "must be greater than or equal to level (Hashtag #{self.name})") unless
+      self.required_by_level.rank >= self.rank
+  end
+
   def rank
     level&.rank || 0
   end
