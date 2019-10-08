ebb6b3fba215a18587d0055d2ce960b9c49c4708
Michael Lamberti <michael.lamberti@devolo.de>
2019-10-09 01:27:27 +0200

    allow set date on photo upload

--- app/graphql/mutations/create_photo.rb
+++ app/graphql/mutations/create_photo.rb
@@ -1,18 +1,20 @@
 module Mutations
   class CreatePhoto < BaseMutation
+    argument :picture, ApolloUploadServer::Upload, required: true
     argument :people_count, Integer, required: true
     argument :hashtag_ids, [ID], required: true, loads: Types::HashtagType
-    argument :picture, ApolloUploadServer::Upload, required: true
+    argument :date, GraphQL::Types::ISO8601DateTime, required: false
 
     field :photo, Types::PhotoType, null: true
     field :errors, [String], null: true
 
-    def resolve(people_count:, hashtags:, picture:)
+    def resolve(picture:, people_count:, hashtags:, date: nil)
       photo = Photo.new(
         user: context[:current_user],
         group: context[:current_user].group,
         people_count: people_count,
-        hashtags: hashtags
+        hashtags: hashtags,
+        date: date
       )
 
       photo.picture.attach(io: picture.to_io, filename:  picture.original_filename)
