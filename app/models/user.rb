7b6f61a2f699f3600e74bde95ca6c0b8fd2cf182
Michael Lamberti <michael.lamberti@devolo.de>
2019-10-10 01:53:08 +0200

    revert add last_seen to users and instead touch user record

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,6 @@ class User < ApplicationRecord
   has_secure_token :auth_token
   enum gender: [ :maschi, :maschine, :pinguin ]
 
-  before_create :default_last_seen_to_now
-
-  def default_last_seen_to_now
-    self.last_seen ||= Time.now
-  end
-
   def invalidate
     self.invalidated = true;
     self.save!;
--- db/migrate/20191009161246_add_last_seen_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLastSeenToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :last_seen, :datetime
-  end
-end
